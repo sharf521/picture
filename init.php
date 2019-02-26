2b87ea6782223c7605aa728dec1c0badd2f1747a
sharf <373455742@qq.com>
2019-02-26 11:17:25 +0800

    session

--- init.php
+++ init.php
@@ -1,13 +1,16 @@
 <?php
 error_reporting(E_ALL & ~E_NOTICE);
 //error_reporting(7);
-session_cache_limiter('public');//缓存必须设置session
-ini_set('session.cookie_domain', '.a.com');
-session_start();
+
 date_default_timezone_set('Asia/Shanghai');//时区配置
 header('Content-type: text/html; charset=utf-8');
 header('X-Powered-By: JAVA');
+header('Cache-control: private, must-revalidate');  //支持页面回跳
 set_time_limit(30);//# 设置执行时间
+
+//session_cache_limiter('public');//缓存必须设置session
+//ini_set('session.cookie_domain', '.a.com');
+session_start();
 //require 'vendor/autoload.php';
 
 define('ROOT', dirname(__FILE__) . '/');
