6080b0eaf57e99647945c702e8b381fe56ea985a
sharf <373455742@qq.com>
2020-11-21 19:53:54 +0800

    ali oss

--- upload.php
+++ upload.php
@@ -8,7 +8,7 @@ if ($base_path == ''){
 $dir = 'data/' . $base_path ;
 $return['status'] = 0;
 
-$filePath =$_POST['path'];
+$filePath =str_replace('//','/',$_POST['path']);
 $_path=trim(dirname($filePath),'.');
 $name=basename($filePath);
 $path = empty($_path) ? $dir : "{$dir}/{$_path}";
@@ -27,7 +27,10 @@ if ($_FILES) {
         if (move_uploaded_file($tmp_name, $file)) {
             $return['status'] = 1;
             $return['path']= $_path . '/' . $name;
-            $return['file'] ='http://'.$_SERVER['HTTP_HOST'].$_path . '/' . $name;    //不包含fenecll
+            //$return['file'] ='http://'.$_SERVER['HTTP_HOST'].$_path . '/' . $name;    //不包含fenecll
+
+            require 'vendor/autoload.php';
+            $return['file'] = \App\Config::$oss['domain'] .saveOSS($return['path']);
         } else {
             $return['error'] = 'Upload file failed';
         }
@@ -36,3 +39,26 @@ if ($_FILES) {
     $return['error'] = 'Upload file is empty！';
 }
 echo json_encode($return);
+
+
+function saveOSS($path)
+{
+    $accessKeyId     = \App\Config::$oss['keyId'];
+    $accessKeySecret = \App\Config::$oss['keySecret'];
+    $endpoint        = \App\Config::$oss['endpoint'];
+    $bucket          = \App\Config::$oss['bucket'];
+    try {
+        $ossClient = new \OSS\OssClient($accessKeyId, $accessKeySecret, $endpoint);
+        $file_name = substr($path, 6);//去除/data
+        $file_path = substr($path, 1);//去除/
+        if(strpos($_SERVER['HTTP_HOST'],'test.cn')!==false){
+            $file_name='test/'.$file_name;
+        }
+        $file_path='data/picture/'.$file_path;
+        $info      = $ossClient->uploadFile($bucket, $file_name, $file_path);
+        $arr       = parse_url($info['oss-request-url']);
+        return $arr['path'];
+    } catch (\OSS\Core\OssException $e) {
+        echo ($e->getMessage());
+    }
+}
